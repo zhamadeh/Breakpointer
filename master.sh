8c0086140ec1ee3ce57e7241c0c311f51122f78a
zhamadeh <zeid.hamadeh@hotmail.com>
2020-09-11 09:46:52 -0700

    update master

--- master.sh
+++ master.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 #take metadata file with libraries characterized as g (good),p (poor), d (shallow) and move to subdir
-Rscript Scripts/createSubDirOfGoodLibraries.R /Users/zeidh/Desktop/Data/METRICS/Aug18.txt  /Users/zeidh/Desktop/Data/RDATA/Aug18-2020/unfiltered_fromServer/ /Users/zeidh/Desktop/Data/RDATA/Aug18-2020/good/
+Rscript Scripts/createSubDirOfGoodLibraries.R Input/metrics/  Input/RData_unfiltered/ Input/RData_good/
 
-cp /Users/zeidh/Desktop/Data/RDATA/Aug18-2020/good/* /Users/zeidh/Desktop/Data/RDATA/ALL/
+Rscript Scripts/blacklistCentromeresFromRData.R Input/RData_good/ Input/RData_blacklisted/ Output/BPR_breaksPlots/
 
-Rscript Scripts/blacklistCentromeresFromRData.R /Users/zeidh/Desktop/Data/RDATA/ALL/ /Users/zeidh/Desktop/Data/RDATA/ALL_BL/ /Users/zeidh/Desktop/Data/PLOTS/
-
-Rscript Scripts/collectBreakpoints.R /Users/zeidh/Desktop/Data/RDATA/ALL_BL /Users/zeidh/Desktop/BreakpointerGithub/Breakpoints/ /Users/zeidh/Desktop/Data/METRICS
+Rscript Scripts/collectBreakpoints.R Input/RData_blacklisted/ Output/Breakpoints/ Input/Metrics/ Aug28_breakpoints.txt
