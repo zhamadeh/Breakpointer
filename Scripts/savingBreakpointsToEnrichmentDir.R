8b21c7f47c7585ae3a1f546a816c84d3eb8dd7b9
zhamadeh <zeid.hamadeh@hotmail.com>
2020-09-14 15:24:54 -0700

    Adding bed files

--- Scripts/savingBreakpointsToEnrichmentDir.R
+++ Scripts/savingBreakpointsToEnrichmentDir.R
@@ -1,5 +1,5 @@
 args = commandArgs(trailingOnly=TRUE)
-
+args=c("Output/Breakpoints/Aug28_breakpoints.txt")
 
 countedSCEs <- read.table(args[1],header=T)
 
@@ -16,16 +16,18 @@ for (row in 1:nrow(countedSCEs)){
 		n=n+1
 	}
 }
-nrow(countedSCEs)
-n
 
-if (length(bl)!=0){
-	q <- r[-c(bl),]
-}
+cat("\n\n There were ",n," breakpoints with an interval greater than 10kb, leaving behind ", (nrow(countedSCEs)-n)," out of",nrow(countedSCEs),"\n\n")
 
+q <- as.data.frame(r[-c(bl),])
 countedSCEs=q
 
-write.table(filter(countedSCEs,gene=="BLM"),"Output/BED_enrichment_analysis/blm.bed",quote=F,col.names = F,row.names = F,sep="\t")
-write.table(filter(countedSCEs,gene=="RECQL5"),"Output/BED_enrichment_analysis/recql5.bed",quote=F,col.names = F,row.names = F,sep="\t")
-write.table(filter(countedSCEs,gene=="BLM/RECQL5"),"Output/BED_enrichment_analysis/blm-recql5.bed",quote=F,col.names = F,row.names = F,sep="\t")
-write.table(filter(countedSCEs,gene=="WT"),"Output/BED_enrichment_analysis/wt.bed",quote=F,col.names = F,row.names = F,sep="\t")
+cat(nrow(dplyr::filter(countedSCEs,countedSCEs$gene=="BLM"))," ... BLM \n")
+cat(nrow(dplyr::filter(countedSCEs,countedSCEs$gene=="BLM/RECQL5"))," ... BLM/RECQL5 \n")
+cat(nrow(dplyr::filter(countedSCEs,countedSCEs$gene=="RECQL5"))," ... RECQL5 \n")
+cat(nrow(dplyr::filter(countedSCEs,countedSCEs$gene=="WT"))," ... WT \n")
+
+write.table(dplyr::filter(countedSCEs,countedSCEs$gene=="BLM"),"Output/BED_enrichment_analysis/blm.bed",quote=F,col.names = F,row.names = F,sep="\t")
+write.table(dplyr::filter(countedSCEs,countedSCEs$gene=="RECQL5"),"Output/BED_enrichment_analysis/recql5.bed",quote=F,col.names = F,row.names = F,sep="\t")
+write.table(dplyr::filter(countedSCEs,countedSCEs$gene=="BLM/RECQL5"),"Output/BED_enrichment_analysis/blm-recql5.bed",quote=F,col.names = F,row.names = F,sep="\t")
+write.table(dplyr::filter(countedSCEs,countedSCEs$gene=="WT"),"Output/BED_enrichment_analysis/wt.bed",quote=F,col.names = F,row.names = F,sep="\t")
